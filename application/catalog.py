5cef5b3a35ec3d4c24c8fe77311eaa084b568131
olof44 <74782170+olof44@users.noreply.github.com>
2020-11-24 15:29:35 +0200

    Update catalog.py

--- application/catalog.py
+++ application/catalog.py
@@ -6,10 +6,12 @@ price2.price()
 price3 = Stock(3, 0)
 price3.price()
 class Catalog:
-    def Catalog(self):
-        print("=========== Catalog ===========")
+    def __init__(self):
+        print("============= Catalog =============")
         print("Pig: " + str(price.v) + "$/kg")
         print("Horse: " + str(price2.v) + "$/kg")
         print("Dog: " + str(price3.v) + "$/kg")
-        print("===============================\n")
+        print("===================================\n")
+
+
 
