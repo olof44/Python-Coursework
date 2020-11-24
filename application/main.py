547040b2a9c8797f287fdbcbe49186d24a9bcbca
olof44 <74782170+olof44@users.noreply.github.com>
2020-11-24 15:31:33 +0200

    Update main.py

--- application/main.py
+++ application/main.py
@@ -1,22 +1,19 @@
-from stock import Stock
 from catalog import Catalog
 from customer import Customer
 #Функциите на Class "Stock" се демонстрират в началото на Class "Customer"
 #За да може класа да използва параметрите на Class "Stock"
 
 cat = Catalog()
-cat.Catalog()
 
 c = Customer("Hristo", 30000, "Pig")
 c.how_much_food_you_need(15, 30)
 c.buy()
-c = Customer("Petyr", 3000, "Dog")
+c = Customer("Petyr", 3000, "Pig")
 c.how_much_food_you_need(3, 30)
 c.buy()
 c = Customer("Joro", 64510, "Horse")
 c.how_much_food_you_need(5, 30)
 c.buy()
-
-
-
-
+c = Customer("Mitko", 25421, "Dog")
+c.how_much_food_you_need(6, 30)
+c.buy()
